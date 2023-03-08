27163277de6d45c1c11c55f90e4f4976981ec64a
mxrgo <126516047+mxrgo@users.noreply.github.com>
2023-03-08 10:25:32 +0100

    chore: add all files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
+    static Scanner read = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("hola");
+
+    }
+
+    public static void Menu(){
+        int opcion = 0;
+        read.nextInt();
+        do {
+            System.out.println("¡Bienvenidx a la aplicación! Elige que quieres hacer");
+            System.out.println("1. Añadir nueva canción");
+            System.out.println("2. Añadir nueva canción");
+        } while(opcion != 6){
+            System.out.println("Saliendo...");
+        }
     }
 }
